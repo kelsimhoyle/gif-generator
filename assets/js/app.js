da36402fcad41c629a74964b97ce9816a963544e
Kelsi H <kelsimhoyle@gmail.com>
2019-05-20 16:36:42 -0600

    when a button is clicked, a function with ajax is run to get the JSON data

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,46 @@
-var topics = ["deal with it", "kermit", "sips tea", "fail", "everything is fine", "you mad", "grumpy cat", "doge", "spongebob"];
+$(document).ready(function () {
+    var topics = ["deal with it", "kermit", "sips tea", "fail", "everything is fine", "you mad", "grumpy cat", "doge", "spongebob"];
+    var searchTerm = "";
+    
 
-// create buttons to put on the page
-function generateButtons(buttonList) {
-    $("#topic-buttons").empty();
-    for (var i = 0; i < buttonList.length; i++) {
-        var currentButton = buttonList[i];
-        var newButton = $("<button>").addClass("gif-button").attr("data-term", currentButton).text(currentButton);
-        $("#topic-buttons").append(newButton);
+    // create buttons to put on the page
+    function generateButtons(buttonList) {
+        $("#topic-buttons").empty();
+        for (var i = 0; i < buttonList.length; i++) {
+            var currentButton = buttonList[i];
+            var newButton = $("<button>").addClass("gif-button click").attr("data-term", currentButton).text(currentButton);
+            $("#topic-buttons").append(newButton);
+        }
+
+        // when the user pushes a button, then it will grab 10 STILL images
+        $(".gif-button").on("click", function(event) {
+            searchTerm = $(this).attr("data-term");
+            generateGifs(searchTerm);
+        });
     }
-}
+    
+    function generateGifs(item) {
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + item + "&api_key=q23oHV6P0B2Ii2IRB8vmBK5rpAMGy3GL&limit=10";
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response){
+            var gifs = response.data;
+            for (var i = 0; i < response.data; i++) {
+                
+            }
+        });
+    }
 
-// when the user submits a new topic, then it is pushed to the topics array
-$("#submit").on("click", function(event){
-    event.preventDefault();
-    topics.push($("#new-topic").val());
+    // when the user submits a new topic, then it is pushed to the topics array
+    $("#submit").on("click", function (event) {
+        event.preventDefault();
+        topics.push($("#new-topic").val());
+        generateButtons(topics);
+        $("#new-topic").val("");
+    });
+
+    // when the user clicks the still image, it will then animate
+
     generateButtons(topics);
-    $("#new-topic").val("");
-})
-
-generateButtons(topics);
+});
